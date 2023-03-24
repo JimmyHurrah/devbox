35d77c5119b903436b1d4b10d66c186512acf9d1
Jimmy Hurrah <jimmyhurrah@users.noreply.github.com>
2023-03-25 00:18:25 +0100

    remake install script

--- install.sh
+++ install.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
+
+# install devbox
 if ! command devbox 2>/dev/null; then
   curl -fsSL https://get.jetpack.io/devbox | bash
 fi
+
+mkdir ~/devbox && cd $_
+
+# install packages
+curl https://raw.githubusercontent.com/jimmyhurrah/devbox/main/devbox.json > devbox.json
 devbox run init
 
-pwsh -c install-module pure-pwsh 
-pwsh -c install-module posh-git 
+# setup dotfiles
+git init
+git remote add origin https://github.com/jimmyhurrah/devbox.git
+git fetch
+git checkout -t origin/main -f
+devbox run dotfiles
--- readme.md
+++ readme.md
@@ -1,4 +1,4 @@
 # devbox
-```
-./install.sh
+```cli
+curl https://raw.githubusercontent.com/jimmyhurrah/devbox/main/install.sh | bash
 ```
