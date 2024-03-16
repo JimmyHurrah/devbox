69f32fee8ebf7a7e1c04f90e6e5c8e1bdc41b951
Jimmy Hurrah <jimmyhurrah@users.noreply.github.com>
2024-03-16 14:17:21 +0100

    add github copilot to neovim

--- dotfiles/home/.config/nvim/init.lua
+++ dotfiles/home/.config/nvim/init.lua
@@ -103,6 +103,8 @@ require("lazy").setup({
 
 	{ "m4xshen/autoclose.nvim", opts = {} },
 
+	{ "github/copilot.vim", opts = {} },
+
 	{ -- Adds git related signs to the gutter, as well as utilities for managing changes
 		"lewis6991/gitsigns.nvim",
 		opts = {
